7ef99f76fe9717009676424e5d06e886dd14f2c9
kebapcore <samilnurdoganforsocial@gmail.com>
2026-01-27 18:02:10 +0300

    Add files via upload

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zpqcfjgfchugpuhqtuzb.supabase.co';
-const supabaseAnonKey = 'sb_publishable_RDkCmly0mYk_XtANN408Fg_F1GUv1SD';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
